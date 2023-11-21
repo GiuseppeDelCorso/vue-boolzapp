1d9b40df9e18d8aecf729d35c42e68618193d8f0
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-21 18:24:32 +0100

    sv

--- main.js
+++ main.js
@@ -10,17 +10,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30:55',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50:00',
                             message: 'Ricordati di stendere i panni',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 16:15:22',
+                            date: '16:15:22',
                             message: 'Tutto fatto!',
                             status: 'received'
                         }
